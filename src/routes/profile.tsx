147ab6552d0dcb3381d91ad8f1a8846583f7893b
Eva Frankovic <emfrankovic7@gmail.com>
2025-11-03 14:46:25 -0500

    Error messages working on submit, now need to update styling

--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -11,20 +11,20 @@ export const Route = createFileRoute("/profile")({
 
 const formSchema = z.object({
   email: z.string().email({ message: "Invalid email address." }),
-  role: z.array(z.string()).min(1, { message: "Please select at least one role." }),
-  degree: z.array(z.string()).min(1, { message: "Please select a degree." }),
-  degreeField: z.string().min(1, { message: "Please enter a degree field." }),
-  timeZone: z.array(z.string()).min(1, { message: "Please select a time zone." }),
+  role: z.string().min(1, { message: "Please select at least one role." }),
+  degree: z.string().min(1, { message: "Please select a degree." }),
+  degreeField: z.string().min(1, {message: "Please enter your degree field"}),
+  timeZone: z.string().min(1, { message: "Please select a time zone." }),
 });
 
 function Profile() {
   const form = useAppForm({
     defaultValues: {
       email: "",
-      role: [] as string[],
-      degree: [] as string[],
+      role: "",
+      degree: "",
       degreeField: "",
-      timeZone: [] as string[],
+      timeZone: "",
     },
     validators: {
       onSubmit: formSchema,
