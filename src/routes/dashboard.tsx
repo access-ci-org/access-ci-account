08ad8c4c6535359a94e1823a5ed19e49ed4e1583
Eva Frankovic <emfrankovic7@gmail.com>
2025-11-10 14:47:26 -0500

    Refactored cards added link functionality

--- src/routes/dashboard.tsx
+++ src/routes/dashboard.tsx
@@ -1,20 +1,14 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { siteTitle } from "@/config";
 
-import { Link } from "@tanstack/react-router";
+
 import { CheckCircle2Icon } from "lucide-react"
 import {
   Alert,
   AlertDescription,
   AlertTitle,
 } from "@/components/ui/alert"
-import {
-  Card,
-  CardAction,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
+import { DashboardCard } from "@/components/dashboard-cards";
 
 export const Route = createFileRoute("/dashboard")({
   component: Dashboard,
@@ -40,100 +34,44 @@ function Dashboard() {
             </AlertDescription>
           </Alert>
         </div>
-
         <div className="grid grid-flow-col items-center pt-5 gap-10">
-          <Link to="/profile" className="block">
-            <Card>
-              <CardHeader>
-                <CardTitle> Profile </CardTitle>
-                <CardDescription>
-                  Edit, update, or add to your profile.
-                </CardDescription>
-                <CardAction>
-                </CardAction>
-              </CardHeader>
-            </Card>
-          </Link>
-
-          <Link to="/profile" className="block">
-            <Card>
-              <CardHeader>
-                <CardTitle> Projects </CardTitle>
-                <CardDescription>
-                  Find your allocations and projects.
-                </CardDescription>
-                <CardAction>
-                </CardAction>
-              </CardHeader>
-            </Card>
-          </Link>
-
-          <Link to="/profile" className="block">
-            <Card>
-              <CardHeader>
-                <CardTitle> SSH Keys </CardTitle>
-                <CardDescription>
-                  Log into your resources securely. Find user names/SSH keys.
-                </CardDescription>
-                <CardAction>
-                </CardAction>
-              </CardHeader>
-            </Card>
-          </Link>
-
-          <Link to="/profile" className="block">
-            <Card>
-              <CardHeader>
-                <CardTitle> Metrics </CardTitle>
-                <CardDescription>
-                  Use XDMoD to track your project's performance and efficiency.
-                </CardDescription>
-                <CardAction>
-                </CardAction>
-              </CardHeader>
-            </Card>
-          </Link>
+          <DashboardCard
+            title="Profile"
+            description="Edit, update, or add to your profile."
+            to="/profile"
+          />
+          <DashboardCard
+            title="Projects"
+            description="Find your allocations and projects."
+            to="https://allocations.access-ci.org/"
+          />
+          <DashboardCard
+            title="SSH Keys"
+            description="Log into your resources securely. Find user names/SSH keys."
+            to="/ssh-keys"
+          />
+          <DashboardCard
+            title="Metrics"
+            description="Use XDMoD to track your project's performance and efficiency."
+            to="https://metrics.access-ci.org/"
+          />
         </div>
-
-        <div className="grid grid-flow-col items-center pb-5 gap-10">
-          <Link to="/profile" className="block">
-            <Card>
-              <CardHeader>
-                <CardTitle> Publications </CardTitle>
-                <CardDescription>
-                  Add your paper, dataset, software, dissertation, or other published work.
-                </CardDescription>
-                <CardAction>
-                </CardAction>
-              </CardHeader>
-            </Card>
-          </Link>
-
-          <Link to="/profile" className="block">
-            <Card>
-              <CardHeader>
-                <CardTitle> Community Persona </CardTitle>
-                <CardDescription>
-                  Tag your skills and interests. Find affinity groups, contributions, and event registrations.
-                </CardDescription>
-                <CardAction>
-                </CardAction>
-              </CardHeader>
-            </Card>
-          </Link>
-
-          <Link to="/profile" className="block">
-            <Card>
-              <CardHeader>
-                <CardTitle> Help Tickets </CardTitle>
-                <CardDescription>
-                  Find Help Tickets associated with your account.
-                </CardDescription>
-                <CardAction>
-                </CardAction>
-              </CardHeader>
-            </Card>
-          </Link>
+        <div className="grid grid-flow-col items-center pt-5 gap-10">
+        <DashboardCard
+            title="Publications"
+            description="Add your paper, dataset, software, dissertation, or other publihsed work."
+            to="https://allocations.access-ci.org/publications"
+          />
+          <DashboardCard
+            title="Community Persona"
+            description="Tag your skills and interests. Find affinity groups, contributions, and event registrations."
+            to="https://support.access-ci.org/community-persona"
+          />
+          <DashboardCard
+            title="Help Tickets"
+            description="Find Help Tickts assocaited with your account."
+            to="https://access-ci.atlassian.net/servicedesk/customer/user/requests"
+          />
         </div>
       </div>
     </>
