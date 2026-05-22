6bd411ae8b2a60ef13bc805165491c762a920ea4
Eva Frankovic <emfrankovic7@gmail.com>
2026-05-22 12:21:12 -0400

    Implemented way to display links on SSH Key page

--- src/routes/ssh-keys.tsx
+++ src/routes/ssh-keys.tsx
@@ -10,6 +10,7 @@ import {
 import { useNavigate } from "@tanstack/react-router";
 
 import { FaKey } from "react-icons/fa";
+import { BsQuestionCircleFill } from "react-icons/bs";
 import { Button } from "@/components/ui/button";
 import { FieldSeparator } from "@/components/ui/field";
 import ButtonRow from "@/components/button-row";
@@ -28,14 +29,42 @@ function SSHKeysRoute() {
 
   return (
     <div className="w-full mt-4">
-      <div className="flex w-full items-center justify-between gap-4 mb-2">
-        <h1>SSH Keys</h1>
-        <Button
-          disabled={isImpersonating}
-          onClick={() => navigate({ to: "/add-ssh-key" })}
-        >
-          Add SSH Key
-        </Button>
+      <div className="flex w-full items-center justify-between mb-2">
+        <div className="flex items-center gap-4">
+          <h1>SSH Keys</h1>
+          <div className="group relative inline-flex">
+            <BsQuestionCircleFill className="cursor-pointer !text-2xl text-[#1a5b6e] hover:text-[#2a7f96]" />
+            <div className="absolute left-0 top-full z-50 hidden w-73 max-w-sm rounded border bg-white p-3 shadow group-hover:block">
+              <p className="!text-lg mb-2">Need some help with SSH Keys?</p>
+              <a
+                href="https://www.youtube.com/watch?v=kkEnezr6MWo"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="block text-blue-600 underline hover:text-blue-400"
+              >
+                Introduction to SSH Keys
+              </a>
+              <a
+                href="https://www.youtube.com/watch?v=DjTBQykeg0c"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="block text-blue-600 underline mt-1 hover:text-blue-400"
+              >
+                Creating SSH Keys
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <Button
+            disabled={isImpersonating}
+            onClick={() => navigate({ to: "/add-ssh-key" })}
+          >
+            Add SSH Key
+          </Button>
+
+        </div>
       </div>
 
       <FieldSeparator />
